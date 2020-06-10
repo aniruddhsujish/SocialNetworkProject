a9aef93897a661d13ed62e8b63c7f0efdf55a74a
aniruddhsujish <aniruddhsujish1113@gmail.com>
2020-06-10 14:52:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -568,5 +568,5 @@ router.post('/uploadProfile', userAuth.isAuthenticated, function(req, res)
 });
 server.listen(process.env.PORT || 3900,process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("Server listening at", addr.address + ":" + addr.port);
 });
